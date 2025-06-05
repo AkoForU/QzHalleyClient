4008feeed82e052db3d00690fd9745adfa78d0ae
Ako <silovetv04@gmail.com>
2025-06-05 15:16:55 +0300

    added admin interface, adding page for dashboard and for adding users design

--- Main/AdminDashboard.xaml.cs
+++ Main/AdminDashboard.xaml.cs
@@ -10,28 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Main
 {
     /// <summary>
-    /// Interaction logic for AminPage.xaml
+    /// Interaction logic for AdminDashboard.xaml
     /// </summary>
-    public partial class AminPage : Window
+    public partial class AdminDashboard : Page
     {
-        public AminPage()
+        public AdminDashboard()
         {
             InitializeComponent();
-            ResizeMode = ResizeMode.NoResize;
-        }
-
-        private void Loaded(object sender, RoutedEventArgs e)
-        {
-            Frame adminFrame = new Frame();
-            adminFrame.Name = "AdminPage";
-            MainWindow.Children.Clear();
-            MainWindow.Children.Add(adminFrame);
-            adminFrame.Navigate(new AdminDashboard());
         }
     }
 }
